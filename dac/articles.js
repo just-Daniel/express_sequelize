7d1e6a84fd1394744815f04608e3f02be3bc3106
Your Name <you@example.com>
2020-04-22 20:11:06 +0300

    change articles

--- dac/articles.js
+++ dac/articles.js
@@ -1,34 +1,60 @@
 const ResponseError = require('../routes/auth/response-error');
 const Article = require('../models/article');
 
+// const updateArticles = (id, title, body, decodedId) =>{
+//   const promise = new Promise((resolve, reject) => {
+//     db.query('SELECT * FROM articles WHERE id = ?', [id], (err, rows, fields) =>{
+//       if (err) {
+//         reject(new ResponseError(err, 400));
+//       } else {
+//         if (rows[0].user_id === decodedId) {
+//           db.query(`UPDATE articles SET title= ?, body= ?, updated_date= ? WHERE id= ?`,
+//               [title, body, new Date(), id], (err, rows, fields) => {
+//                 if (err) {
+//                   reject(new ResponseError('Unable to update article ' + err, 400));
+//                 } else {
+//                   if (rows.length === 0) {
+//                     reject(new ResponseError(`Id: "${id}" not found ` + err, 400));
+//                   } else {
+//                     resolve({status: 'OK'});
+//                   }
+//                 }
+//               });
+//         } else {
+//           reject(new ResponseError(
+//               `User doesn't have permissions to edit this articles! ` + err, 400));
+//         }
+//       }
+//     });
+//   });
+//   return promise;
+// };
+
+
 const updateArticles = (id, title, body, decodedId) =>{
   const promise = new Promise((resolve, reject) => {
-    db.query('SELECT * FROM articles WHERE id = ?', [id], (err, rows, fields) =>{
-      if (err) {
-        reject(new ResponseError(err, 400));
-      } else {
-        if (rows[0].user_id === decodedId) {
-          db.query(`UPDATE articles SET title= ?, body= ?, updated_date= ? WHERE id= ?`,
-              [title, body, new Date(), id], (err, rows, fields) => {
-                if (err) {
-                  reject(new ResponseError('Unable to update article ' + err, 400));
-                } else {
-                  if (rows.length === 0) {
-                    reject(new ResponseError(`Id: "${id}" not found ` + err, 400));
-                  } else {
-                    resolve({status: 'OK'});
-                  }
-                }
-              });
+     Article.findByPk(id)
+      .then((articles) => {
+        if(articles[0].user_id === decodedId){  // maybe articles.user_id
+          Article.update({ title: title, body: body,}, { where: { id: id }})
+            .then((articles) => {
+              if (articles.length === 0) {
+                reject(new ResponseError(`Id: "${id}" not found ` + err, 400));
+              } else {
+                resolve({status: 'OK'});
+              }
+            })
+            .catch(err => reject(new ResponseError('Unable to update article ' + err, 400)))
         } else {
           reject(new ResponseError(
-              `User doesn't have permissions to edit this articles! ` + err, 400));
+            `User doesn't have permissions to edit this articles! ` + err, 400));
         }
-      }
-    });
+      })
+      .catch(err => reject(new ResponseError(err, 400)));
   });
   return promise;
 };
+
 
 // const insertArticles = (title, body, decodedId) =>{
 //   const promise = new Promise((resolve, reject) => {
